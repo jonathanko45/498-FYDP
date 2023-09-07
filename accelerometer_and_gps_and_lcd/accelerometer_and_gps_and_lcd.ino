ae467126f5f644bff2b97f8bf9739e6c027977aa
Brandon Phu <brandonphu@outlook.com>
2023-09-06 22:20:29 -0400

    added accelerometer_and_gps_and_lcd sketch
    - this new sketch simply prints the LAT and LONG coordinates from the gps onto the LCD screen
    - this sketch is used to see if the LCD screen will keep updating if serial communication/monitor freezes

--- accelerometer_and_gps_and_lcd/accelerometer_and_gps_and_lcd.ino
+++ accelerometer_and_gps_and_lcd/accelerometer_and_gps_and_lcd.ino
@@ -1,3 +1,53 @@
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ modified 7 Nov 2016
+ by Arturo Guadalupi
+
+ This example code is in the public domain.
+
+ https://docs.arduino.cc/learn/electronics/lcd-displays
+
+*/
+
+// LCD library
+#include <LiquidCrystal.h>
+
+// initialize the LCD library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 // GPS library
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
@@ -42,6 +92,11 @@ double accelZ;
 int accelRange;
 
 void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  // lcd.print("hello, world!");
+  
   accelerometerSetup();
 
   gpsSetup();
@@ -138,6 +193,9 @@ void gpsLoop() {
     if (gps.encode(gpsSerial.read())) {
       // call function to print values in CSV format
       printCSVValues9600();
+
+      // call function to print Lat and Long coordinates to LCD display (for debugging)
+      printLatLongLCD();
     }
 
     // If 5000 milliseconds pass and there are no characters coming in
@@ -250,4 +308,14 @@ void checkSerialConnection() {
     delay(100);    //wait 100 millis
     // Serial.begin(9600);  // reenable serial again
   }
+}
+
+void printLatLongLCD() {
+  lcd.setCursor(0, 0);
+  lcd.print(gps.location.lat(),6);
+  
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print(gps.location.lng(),6);
 }
