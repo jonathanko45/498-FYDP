ee678cba658ce144d32ca108f1dbc8cce59e735e
Brandon Phu <brandonphu@outlook.com>
2024-01-27 20:20:47 -0500

    added updated CAN/SPI sketches

--- Sketches/CAN/CAN stuff/CAN_send/CAN_send.ino
+++ Sketches/CAN/CAN stuff/CAN_send/CAN_send.ino
@@ -12,15 +12,13 @@
 // Channel 0 SPI_CS Pin: BCM 8
 // Channel 1 SPI_CS Pin: BCM 7
 // Interupt Pin: BCM25
-const int SPI_CS_PIN  = BCM8;
-const int CAN_INT_PIN = BCM25;
+const int SPI_CS_PIN  = 10;
 #else
 
 // For Arduino MCP2515 Hat:
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 10;
-const int CAN_INT_PIN = 2;
 #endif
 
 
@@ -37,15 +35,13 @@ mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 int input = 0;                           
 
 void setup() {
-    // SERIAL_PORT_MONITOR.begin(115200);
     Serial.begin(115200);
-
+    delay(2000);
+    Serial.println("HELLO");
     while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
-        // SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
         Serial.println("CAN init fail, retry...");
         delay(100);
     }
-    // SERIAL_PORT_MONITOR.println("CAN init ok!");
     Serial.println("CAN init ok!");
 }
 
@@ -55,10 +51,8 @@ void loop() {
   
 
     if(Serial.available()){
-    // SERIAL_PORT_MONITOR.print("CAN SEND Count data = ");
     Serial.print("CAN SEND Count data = ");
      input = Serial.read() - '0';
-        //  SERIAL_PORT_MONITOR.println( input);
          Serial.println( input);
     uint8_t data_count[] = { CAN_DATA_COUNT, (input >> 16) & 0xFF, (input >> 8) & 0xFF, input & 0xFF };
     // send data:  id = 0x70, standard frame, data len = 8, stmp: data buf
